8c125c0ce72a8e5c600058f972fbf3e8ab4eeeb1
MaxMana-Corner <maxmana.corner@gmail.com>
2026-09-03 01:04:34 -0400

    Update sw.js

--- london-fishing-companion/sw.js
+++ london-fishing-companion/sw.js
@@ -1,5 +1,5 @@
 /* Offline cache. Bump CACHE when you redeploy so phones pick up the new build. */
-const CACHE = "lfc-v6";
+const CACHE = "lfc-v7";
 const ASSETS = [
   "./", "./index.html", "./app.js", "./manifest.webmanifest", "./privacy.html",
   "./icon-180.png", "./icon-192.png", "./icon-512.png"
